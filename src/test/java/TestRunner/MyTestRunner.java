0fb85a9270ef83f44b1b9c8c68dde028276f23e7
VattiguntaSunandha <sunandha26@gmail.com>
2022-06-15 14:49:30 -0400

    lastcomm

--- src/test/java/TestRunner/MyTestRunner.java
+++ src/test/java/TestRunner/MyTestRunner.java
@@ -1,33 +1,19 @@
 package TestRunner;
 
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-<<<<<<< HEAD
-=======
 	@RunWith(Cucumber.class) 
-	@CucumberOptions(features ="./src/test/resources/features",glue= {"StepDefinitions","AppHooks"},
-	          plugin={"pretty","html:target/HtmlReports","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-	          monochrome = true,
-			  dryRun = false,
-			  strict = true
+	@CucumberOptions(features =".\\src/test/resources/com/features",
+	glue= {"stepdefinitions","AppHooks"},
+	plugin={"pretty","html:target/HtmlReports","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+    monochrome = true,
+	dryRun = false,
+		//	  strict = true
+			  publish = true
 			 	 )
 	public class MyTestRunner {
->>>>>>> refs/heads/master
 
-	@RunWith(Cucumber.class)
-	@CucumberOptions(features = {".\\src/test/resources/com/features"}, glue = { "stepdefinitions", "AppHooks" },
-			plugin={"pretty","html:target/HtmlReports","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-			monochrome = true,
-			dryRun = false
-			// strict = true
-	)
-	
-public class MyTestRunner {
 	}
-<<<<<<< HEAD
-	
-=======
-
->>>>>>> refs/heads/master
