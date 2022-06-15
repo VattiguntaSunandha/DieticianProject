388e5aad82a41a068d493f52ddcbc980546ff66b
geeta <geeta.thakur2008@gmail.com>
2022-06-15 14:57:59 -0400

    first commit

--- src/main/java/TestRunner/MyTestRunner.java
+++ src/main/java/TestRunner/MyTestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 
 	@RunWith(Cucumber.class) 
 	@CucumberOptions
-	         (features ={"src/test/resources/com/features"},
+	         (features ={"./src/test/resources/com/features"},
 			 glue= {"stepdefinitions","AppHooks"},
 		      plugin = {"pretty","timeline:test-output-thread/"},
 		    		//"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"//
