831c6390a4ac730d48095492e67b5a8e62753408
swati <swati@SwatiP>
2022-06-15 15:52:16 -0400

    full and final commit

--- src/test/java/TestRunner/MyTestRunner.java
+++ src/test/java/TestRunner/MyTestRunner.java
@@ -5,15 +5,15 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-public class MyTestRunner {
+
 	@RunWith(Cucumber.class) 
-	@CucumberOptions(features ="./src/test/resources/features",glue= {"StepDefinitions","AppHooks"},
-	          plugin={"pretty","html:target/HtmlReports","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+	@CucumberOptions(features ={"src/test/resources/com/features/SignInPageTitle.feature"},glue= {"stepdefinitions","AppHooks"},
+	          plugin={"pretty","html:target/HtmlReports.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 	          monochrome = true,
-			  dryRun = false,
-			  strict = true
+			  dryRun = false
+			  
 			 	 )
-	public class TestRunner {
+	public class MyTestRunner {
 
 	}
-}
+
--- src/test/java/stepdefinitions/TestGit_Swati.java
+++
@@ -1,5 +0,0 @@
-package stepdefinitions;
-
-public class TestGit_Swati {
-
-}
